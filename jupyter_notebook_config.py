0f70b9888be394aca880666f271331805d7f6c18
Aleksander Binion <alek.binion@mongodb.com>
2021-04-14 11:52:23 -0400

    working config

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -2,34 +2,36 @@ import os
 import pymongo
 import json
 import atlas_jupyter as a
-
+​
 #populate the file system on start with contents of mongocollection
 for document in a.notebook_col.find():
     file_name = document["_id"]
     print(file_name) # iterate the cursor
     with open(file_name, 'w') as fp:
         json.dump(document["notebook"]["content"], fp, indent = 1)
-
+​
 def save_to_mongo (model, **kwargs):
-
+​
     # only run on notebooks
     if model['type'] != 'notebook':
         return
     # only run on nbformat v4
     if model['content']['nbformat'] != 4:
         return
-
+​
     # save model to a mongodb collection
     id_dict = {"_id": kwargs["path"]}
     mongo_model = {"notebook" : model}
     mongo_model.update(id_dict)
     a.notebook_col.replace_one(filter = id_dict, replacement = mongo_model, upsert = True)
-
+​
 c.FileContentsManager.pre_save_hook = save_to_mongo
-
+​
+c.NotebookApp.ip = 'localhost'
+c.NotebookApp.port = 8888
+​
 c.NotebookApp.tornado_settings = {
     'headers': {
         'Content-Security-Policy':
-        "frame-ancestors 'self' http://localhost:8080"
+        "frame-ancestors http://localhost:8080 'self'"
     }
-}
