c2d0772092e70201ad14613eb341ea47d2eb3e27
Aleksander Binion <alek.binion@mongodb.com>
2021-04-15 12:29:16 -0400

    remove strange empty character error

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -2,7 +2,7 @@ import os
 import pymongo
 import json
 import atlas_jupyter as a
-​
+
 #populate the file system on start with contents of mongocollection
 for document in a.notebook_col.find():
     file_name = document["_id"]
@@ -16,30 +16,31 @@ for document in a.notebook_col.find():
 def save_to_mongo (model, **kwargs):
     file_name, extension = os.path.splitext(kwargs["path"])
     if extension == ".ipyhistory":
+        file_name = file_name.replace("/", "")
         filter_ = {"_id" : file_name+".ipynb"}
         update = {"$set" : {"notebook.history" : {"history" : model, "path" : kwargs["path"]}}}
         a.notebook_col.update_one(filter=filter_, update=update)
-        return
     # only run on notebooks
     if model['type'] != 'notebook':
         return
     # only run on nbformat v4
     if model['content']['nbformat'] != 4:
         return
-​
+
     # save model to a mongodb collection
     id_dict = {"_id": kwargs["path"]}
     mongo_model = {"notebook" : model}
     mongo_model.update(id_dict)
     a.notebook_col.replace_one(filter = id_dict, replacement = mongo_model, upsert = True)
-​
+
 c.FileContentsManager.pre_save_hook = save_to_mongo
-​
+
 c.NotebookApp.ip = 'localhost'
 c.NotebookApp.port = 8888
-​
+
 c.NotebookApp.tornado_settings = {
     'headers': {
         'Content-Security-Policy':
         "frame-ancestors http://localhost:8080 'self'"
     }
+}
